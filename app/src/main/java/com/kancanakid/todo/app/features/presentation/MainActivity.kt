e275d1b4bc8198b17cb0d1ad4d5e6b415d16ec1e
KancanaKid <b.a.syidiq@gmail>
2023-08-07 12:55:07 +0700

    - Implement splash screen

--- app/src/main/java/com/kancanakid/todo/app/features/presentation/MainActivity.kt
+++ app/src/main/java/com/kancanakid/todo/app/features/presentation/MainActivity.kt
@@ -3,10 +3,12 @@ package com.kancanakid.todo.app.features.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -20,8 +22,16 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel:SplashViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.isLoading.value
+            }
+        }
         setContent {
             TodoAppTheme {
                 // A surface container using the 'background' color from the theme
